28fe1d76f21d1531f7059f7d2c2da93d50ce5a57
ArthurDevLeal <arthurdevleal@gmail.com>
2025-12-30 08:43:54 -0300

    Refactor middleware usage and improve response format in main route

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,31 @@ import helmet from "helmet";
 import { mainRouter } from "./routes/main-router";
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-app.use(helmet());
-app.use(urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "https://lumina.arthurdevleal.tech",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 200, 
+  })
+);
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
   })
 );
 
 app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 app.use(mainRouter);
 
 app.get("/", (req, res) => {
-  res.json("hello world");
+  res.json({ message: "hello world" });
 });
 
 app.listen(PORT, () => {
