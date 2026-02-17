240cb550057691c1e5453110a1bdfc93c46378fd
ArthurDevLeal <arthurdevleal@gmail.com>
2026-02-17 10:09:37 -0300

    Update CORS configuration to change allowed origin and clean up options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express, { urlencoded } from "express";
-import helmet from "helmet";
 import { mainRouter } from "./routes/main-router";
 
 const app = express();
@@ -8,26 +7,20 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "https://lumina.arthurdevleal.tech",
+    origin: "https://lumina.vlsolutions.site",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type", 
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-      "Origin"
-    ],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
     exposedHeaders: ["Content-Length", "X-Request-Id"],
     credentials: true,
-    optionsSuccessStatus: 204, 
-    maxAge: 86400, 
-  })
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+  }),
 );
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.options("*", cors());
+app.options("", cors());
 
 // 5. Suas rotas
 app.use(mainRouter);
